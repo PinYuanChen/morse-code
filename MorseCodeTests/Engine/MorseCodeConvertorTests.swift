0d960bf59a719f96e2f3de1a46ee683d01933062
Patrix <champion790713@gmail.com>
2023-12-22 05:32:55 +0800

    Test deliver empty string when no input

--- MorseCodeTests/Engine/MorseCodeConvertorTests.swift
+++ MorseCodeTests/Engine/MorseCodeConvertorTests.swift
@@ -7,6 +7,23 @@
 
 import XCTest
 
-final class MorseCodeConvertorTests: XCTestCase {
+class MorseCodeConvertor {
+    func convertToMorseCode(input: String) -> String {
+        return ""
+    }
+}
 
+final class MorseCodeConvertorTests: XCTestCase {
+    
+    func test_deliverEmptyString_whenNoInput() {
+        let sut = makeSUT()
+        XCTAssertEqual("", sut.convertToMorseCode(input: ""))
+    }
+    
+    // MARK: - Helpers
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> MorseCodeConvertor {
+        let sut = MorseCodeConvertor()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
