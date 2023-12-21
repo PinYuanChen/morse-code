e3d40c9b12f91a930dfd11fcfc6d22b5f2b25f49
Patrix <champion790713@gmail.com>
2023-12-22 05:20:28 +0800

    Create `MorseCodeConvertorTests`

---
+++ MorseCodeTests/MorseCodeConvertorTests.swift
@@ -0,0 +1,12 @@
+//
+//  MorseCodeConvertorTests.swift
+//  MorseCodeTests
+//
+//  Created by Champion Chen on 2023/12/22.
+//
+
+import XCTest
+
+final class MorseCodeConvertorTests: XCTestCase {
+
+}
