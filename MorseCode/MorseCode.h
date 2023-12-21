3076f49fa972bf8209a29f29549a4522acbf2056
Patrix <champion790713@gmail.com>
2023-12-22 04:57:06 +0800

    Initial Commit

---
+++ MorseCode/MorseCode.h
@@ -0,0 +1,18 @@
+//
+//  MorseCode.h
+//  MorseCode
+//
+//  Created by Champion Chen on 2023/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MorseCode.
+FOUNDATION_EXPORT double MorseCodeVersionNumber;
+
+//! Project version string for MorseCode.
+FOUNDATION_EXPORT const unsigned char MorseCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MorseCode/PublicHeader.h>
+
+
