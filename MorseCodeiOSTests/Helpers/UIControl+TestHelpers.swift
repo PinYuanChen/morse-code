edc911ef7d06f55a812aaf5e4dc0f258a8dd1518
Patrick <champion790713@gmail.com>
2023-12-25 14:14:14 +0800

    Add test helpers to simulate tapping button

---
+++ MorseCodeiOSTests/Helpers/UIControl+TestHelpers.swift
@@ -0,0 +1,15 @@
+//
+// Created on 2023/12/25.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
