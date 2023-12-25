6d01e870674dc5b1043e9a4e875fc2d77926b51d
Patrick <champion790713@gmail.com>
2023-12-25 17:14:03 +0800

    Add empty MorseCodeApp project

---
+++ MorseCodeApp/MorseCodeApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// Created on 2023/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
