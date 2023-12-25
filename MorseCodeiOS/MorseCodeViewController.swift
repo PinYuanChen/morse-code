7c34729ad99d9702a2890935690ca7e45d503255
Patrick <champion790713@gmail.com>
2023-12-25 11:54:35 +0800

    Inject convertor to vc on initialization

--- MorseCodeiOS/MorseCodeViewController.swift
+++ MorseCodeiOS/MorseCodeViewController.swift
@@ -3,7 +3,19 @@
 //
 
 import UIKit
+import MorseCode
 
 public class MorseCodeViewController: UIViewController {
     
+    public required init(convertor: MorseCodeConvertorPrototype) {
+        self.convertor = convertor
+        super.init()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let convertor: MorseCodeConvertorPrototype
 }
--- MorseCodeiOSTests/MorseCodeViewControllerTests.swift
+++ MorseCodeiOSTests/MorseCodeViewControllerTests.swift
@@ -3,14 +3,26 @@
 //
 
 import XCTest
+import MorseCode
 import MorseCodeiOS
+
+class ConvertorSpy: MorseCodeConvertorPrototype {
+    func convertToMorseCode(input: String) -> String {
+        return ""
+    }
+    
+    func convertToMorseFlashSignals(input: String) -> [MorseCode.FlashType] {
+        return []
+    }
+}
 
 final class MorseCodeViewControllerTests: XCTestCase {
 
     
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> MorseCodeViewController {
-        let sut = MorseCodeViewController()
+        let convertor = ConvertorSpy()
+        let sut = MorseCodeViewController(convertor: convertor)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
