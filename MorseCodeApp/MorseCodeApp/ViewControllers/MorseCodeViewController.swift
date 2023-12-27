8862975a3269b6e445de460795267844bc171222
Patrick <champion790713@gmail.com>
2023-12-27 14:52:07 +0800

    Arrange folders and separate custom text field

--- MorseCodeApp/MorseCodeApp/ViewControllers/MorseCodeViewController.swift
+++ MorseCodeApp/MorseCodeApp/ViewControllers/MorseCodeViewController.swift
@@ -187,17 +187,3 @@ private extension MorseCodeViewController {
         let _ = convertor.convertToMorseFlashSignals(input: currentMorseText)
     }
 }
-
-class CustomTextField: UITextField {
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        for view in subviews {
-            if let button = view as? UIButton {
-                button.setImage(button.image(for: .normal)?.withRenderingMode(.alwaysTemplate), for: .normal)
-                button.tintColor = .bg275452
-            }
-        }
-    }
-}
