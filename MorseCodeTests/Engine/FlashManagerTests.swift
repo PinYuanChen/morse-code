ea91a9155d2bae170cb3404a85e5e4c82844c01c
Patrick <champion790713@gmail.com>
2023-12-28 13:05:31 +0800

    Init FlashManager does not start signaling

--- MorseCodeTests/Engine/FlashManagerTests.swift
+++ MorseCodeTests/Engine/FlashManagerTests.swift
@@ -3,7 +3,35 @@
 //
 
 import XCTest
+import MorseCode
+
+class FlashManager {
+    
+    var currentStatus: StatusType = .stop
+    
+    enum StatusType {
+        case playing
+        case pause
+        case stop
+    }
+    
+    func startPlaySignals() {
+        currentStatus = .playing
+    }
+}
 
 final class FlashManagerTests: XCTestCase {
     
+    func test_initDoesNotPlay() {
+        let sut = makeSUT()
+        
+        XCTAssertEqual(sut.currentStatus, FlashManager.StatusType.stop)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FlashManager {
+        let sut = FlashManager()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
