0b471f3949aa7f95400dbf0baab69514a55890dd
Patrix <champion790713@gmail.com>
2024-05-15 14:36:24 +0800

    Construction inject prototypes to presenter

--- MorseCodeApp/MorseCodeApp/Main/Presenter/MorseCodePresenter.swift
+++ MorseCodeApp/MorseCodeApp/Main/Presenter/MorseCodePresenter.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import MorseCode
+
+public class MorseCodePresenter {
+    
+    public required init(convertor: MorseCodeConvertorPrototype, flashManager: FlashManagerPrototype) {
+        self.convertor = convertor
+        self.flashManager = flashManager
+    }
+    
+    // MARK: Private properties
+    private let convertor: MorseCodeConvertorPrototype
+    private var flashManager: FlashManagerPrototype
+}
