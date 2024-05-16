ca0f5ed07db063589dee7ab37c75e90a7cf9373d
Patrix <champion790713@gmail.com>
2024-05-16 18:38:34 +0800

    Save command does not request cache insertion on deletion error

--- MorseCodeTests/MorseCache/CacheMorseRecordUseCaseTests.swift
+++ MorseCodeTests/MorseCache/CacheMorseRecordUseCaseTests.swift
@@ -10,9 +10,14 @@ import MorseCode
 
 class MorseRecordStore {
     var deleteCachedRecordsCallCount = 0
+    var insertCallCount = 0
     
     func deleteCachedRecords() {
         deleteCachedRecordsCallCount += 1
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        
     }
 }
 
@@ -46,6 +51,17 @@ final class CacheMorseRecordUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCachedRecordsCallCount, 1)
     }
     
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let records = [uniqueRecord(), uniqueRecord()]
+        let (sut, store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(records)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalMorseRecordLoader, store: MorseRecordStore) {
         let store = MorseRecordStore()
@@ -57,5 +73,9 @@ final class CacheMorseRecordUseCaseTests: XCTestCase {
     
     private func uniqueRecord() -> MorseRecord {
         return .init(id: UUID(), text: "any", morseCode: "any", flashSignals: [])
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }
