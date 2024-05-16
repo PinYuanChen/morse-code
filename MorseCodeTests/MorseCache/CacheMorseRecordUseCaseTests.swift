3a9c5f8c99d89a2b663b55906bbe10c0483c220e
Patrix <champion790713@gmail.com>
2024-05-16 18:12:17 +0800

    Create CacheMorseRecordUseCaseTests

---
+++ MorseCodeTests/MorseCache/CacheMorseRecordUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  CacheMorseRecordUseCaseTests.swift
+//  MorseCodeTests
+//
+//  Created by Champion Chen on 2024/5/16.
+//
+
+import XCTest
+
+final class CacheMorseRecordUseCaseTests: XCTestCase {
+    
+}
