c4317de1694c9a4206f33ef32c753928d65b7d25
Patrix <champion790713@gmail.com>
2024-05-16 18:23:58 +0800

    Does not delete cache upon `LocalMorseRecordLoader` creation

--- MorseCodeTests/MorseCache/CacheMorseRecordUseCaseTests.swift
+++ MorseCodeTests/MorseCache/CacheMorseRecordUseCaseTests.swift
@@ -7,6 +7,33 @@
 
 import XCTest
 
+class MorseRecordStore {
+    var deleteCachedRecordsCallCount = 0
+}
+
+class LocalMorseRecordLoader {
+    
+    init(store: MorseRecordStore) {
+        self.store = store
+    }
+    
+    private let store: MorseRecordStore
+}
+
 final class CacheMorseRecordUseCaseTests: XCTestCase {
     
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.deleteCachedRecordsCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalMorseRecordLoader, store: MorseRecordStore) {
+        let store = MorseRecordStore()
+        let sut = LocalMorseRecordLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
 }
