930752150023896c1355e8a7629d2e56806b682d
Patrix <champion790713@gmail.com>
2024-05-20 16:16:35 +0800

    Move `LocalMorseRecord` to its own file

--- MorseCode/MorseCache/MorseRecordStore.swift
+++ MorseCode/MorseCache/MorseRecordStore.swift
@@ -14,17 +14,3 @@ public protocol MorseRecordStore {
     func deleteCachedRecords(completion: @escaping DeletionCompletion)
     func insert(_ records: [LocalMorseRecord], completion: @escaping InsertionCompletion)
 }
-
-public struct LocalMorseRecord: Equatable {
-    public let id: UUID
-    public let text: String
-    public let morseCode: String
-    public let flashSignals: [FlashType]
-    
-    public init(id: UUID, text: String, morseCode: String, flashSignals: [FlashType]) {
-        self.id = id
-        self.text = text
-        self.morseCode = morseCode
-        self.flashSignals = flashSignals
-    }
-}
