81a38bf1cbe0081e807b268c973dcbb444c24f53
Patrix <champion790713@gmail.com>
2024-08-08 17:48:24 +0800

    Extract localization to shared SDK

---
+++ MorseCode/Localization/Localization.swift
@@ -0,0 +1,21 @@
+//
+//  Localization.swift
+//  MorseCode
+//
+//  Created by Champion Chen on 2024/8/8.
+//
+
+import Foundation
+
+public class Localization {
+    
+    private static var bundle: Bundle = {
+        Bundle(for: Localization.self)
+    }()
+    
+    public static func string(forKey key: String, comment: String = "") -> String {
+        return NSLocalizedString(key, bundle: bundle, comment: comment)
+    }
+    
+    private init() { }
+}
